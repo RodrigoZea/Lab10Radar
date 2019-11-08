ae712249dbc44cca4bb27c4aebef00c63c481d51
Rodrigo Zea <zearodrigo37@gmail.com>
2019-11-08 01:36:14 -0600

    agregando helmet y configuraciones para PWA optimization

--- lab10radar/src/components/app.js
+++ lab10radar/src/components/app.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
+import Helmet from 'preact-helmet';
 
 import Header from './header';
 
@@ -20,6 +21,16 @@ export default class App extends Component {
 		return (
 			<div id="app">
 				<noscript>Put Sample code here for execution when JavaScript is Disabled</noscript>
+				<Helmet
+					title="Laboratorio 10 Radar"
+					noscript={[
+						{ innerHTML: `<link rel="stylesheet" type="text/css" href="foo.css" />` }
+					]}
+					link={[
+						{ rel: 'apple-touch-icon', href: '../assets/icons/apple-touch-icon.png' },
+						{ rel: 'icon', sizes: '192x192', href: '../assets/icons/android-chrome-192x192' }
+					]}
+				/>
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
